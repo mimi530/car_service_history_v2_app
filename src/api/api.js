4bc20cbe9a19d875ccce0c07f59ae79911192f82
Michal Domzalski <mimi0192@wp.pl>
2022-06-09 21:27:41 +0200

    updated the images

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import { create } from "apisauce";
 import authStorage from "../auth/storage";
 
 const api = create({
-    baseURL: "http://192.168.1.18/api",
+    baseURL: "http://192.168.1.17/api",
     headers: {
         Accept: "application/json",
     },
